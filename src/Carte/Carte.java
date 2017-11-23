d0205c13e0aca64bdf72d8789a405846fa27f516
AlexandreMARTIN <martinalexandre0496@gmail.com>
2017-11-23 15:58:21 +0100

    1 vers

--- src/Carte/Carte.java
+++ src/Carte/Carte.java
@@ -1,1 +1,23 @@
-qsdgdf
+public class Carte
+{
+	private int carte = new int[12][12];
+
+	public Carte()
+	{
+		int nbJoueur;
+		int nbCoffre;
+		//se renseigner sur les tuples de java
+
+		for(int i = 0; i <= 11; i++)
+		{
+			for(int i = 0; i <= 11; i++)
+			{
+				carte[i][j] = 0;
+			}	
+		}
+		
+		nbJoueur = Joueur.getNbJoueur();
+		nbCoffre = nbJoueur * 3;
+		
+	}
+}
