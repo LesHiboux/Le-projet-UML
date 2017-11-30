6f8a29c4e1f119827a2697194273f499eb1667df
Delfeil Casanova <delfeilcasanova@gmail.com>
2017-11-30 15:39:21 +0100

    Partie

--- src/Game/Partie.java
+++ src/Game/Partie.java
@@ -1,1 +1,39 @@
-sDQFQDS
+
+import java.util.Scanner;
+import java.io.File;
+
+public class Partie;
+{
+
+	//Attributs
+
+	private Carte carte;
+	
+	private LinkedList<Joueur> fileJoueurs;
+	private int nbJoueurs;
+	
+	private LinkedList<Pirate> filePirates;
+	private int nbPirates;
+
+	private LinkedList<Coffre> listeCoffres;
+	private int nbCoffres;
+
+	private int tresorPosX;
+	private int tresorposY;
+
+	//Fonctions
+	public boolean sauvegarde()
+	{
+		String name;
+		String dossier="./Sauvegardes/";
+		Scanner saisie = new Scanner(System.in);
+		System.out.println("Saisissez le nom de la sauvegarde");
+		name=saisie.readLine();
+		dossier+=name;
+		File dossierSave = new File(dossier);
+		if(dossierSave.exists()==false)
+		{
+			dossierSave.mkdirs();
+		}
+	}
+}
