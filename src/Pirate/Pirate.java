c1b8a6cfe8ee5382c189760a290171c90ff85109
Vincent de Germiny <vdegerminy@gmail.com>
2017-11-30 15:41:07 +0100

    Update Pirate.java

--- src/Pirate/Pirate.java
+++ src/Pirate/Pirate.java
@@ -1,1 +1,13 @@
-sdhghd
+import java.util.Random;
+public class Pirate {
+	private int posx;
+	private int posy;
+	private boolean vivant;
+  
+  public void deplacer() {
+    
+  }
+  public void attaquer() {
+
+	}
+}
