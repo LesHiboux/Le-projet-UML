2e2a7f4dfedfc3f8b7eb8e70b716eeb56956806f
AlexandreMARTIN <martinalexandre0496@gmail.com>
2017-12-07 10:56:16 +0100

    Update Pair.java

--- src/Partie/Pair.java
+++ src/Partie/Pair.java
@@ -1,31 +1,31 @@
 
-public class Pair<L,R>
+public class Pair
   {
 
-  private int L left;
-  private int R right;
+    private int left;
+    private int right;
 
-  public Pair(int left, int right)
-  {
-    this.left = left;
-    this.right = right;
-  }
+    public Pair(int left, int right)
+    {
+      this.left = left;
+      this.right = right;
+    }
 
-  public int getLeft()
-  {
-    return left;
-  }
-  
-  public int getRight()
-  {
-    return right;
-  }
+    public int getLeft()
+    {
+      return left;
+    }
 
-  public boolean equality(Object o)
-  {
-    if (!(o instanceof Pair)) return false;
-    Pair pairo = (Pair) o;
-    return this.left.equality(pairo.getLeft()) && this.right.equality(pairo.getRight());
-  }
+    public int getRight()
+    {
+      return right;
+    }
+
+    public boolean equality(Object o)
+    {
+      if (!(o instanceof Pair)) return false;
+      Pair pairo = (Pair) o;
+      return this.left.equality(pairo.getLeft()) && this.right.equality(pairo.getRight());
+    }
 
 }
