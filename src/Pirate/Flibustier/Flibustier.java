3b57ff774412852b511a1a5765a5f3ef72afb8d2
Delfeil Casanova <delfeilcasanova@gmail.com>
2017-12-07 10:04:37 +0100

    Add files via upload

--- src/Pirate/Flibustier/Flibustier.java
+++ src/Pirate/Flibustier/Flibustier.java
@@ -1,3 +1,13 @@
 public class Flibustier extends Pirate {
 
+	public Flibustier(String strFlib)
+	{
+		super(strFlib);
+		nbFlib++;
+	}
+
+	public String toString()
+	{
+		return "1|" + posX + "|" + posY + "|";
+	}
 }
