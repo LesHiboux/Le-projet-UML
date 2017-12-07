3540dd7e4bf84c8926d96069154e011ea2bee8fa
Delfeil Casanova <delfeilcasanova@gmail.com>
2017-12-07 10:05:00 +0100

    Add files via upload

--- src/Pirate/Boucanier/Boucanier.java
+++ src/Pirate/Boucanier/Boucanier.java
@@ -1,3 +1,12 @@
 public class Boucanier extends Pirate {
+	public Flibustier(String strBouc)
+	{
+		super(strBouc);
+		nbBouc++;
+	}
 
+	public String toString()
+	{
+		return "2|" + posX + "|" + posY + "|";
+	}
 }
